f1ad9c9ed73ca04ac51a729adbc760116964579c
Zoe <ezoe013@gmail.com>
2020-05-18 17:47:10 +0100

    Adding templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 
 def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now().order_by('published_date'))
 	return render(request, 'blog/post_list.html', {})
 
 	#Creating a function (def) called post_list. 
@@ -11,3 +14,4 @@ def post_list(request):
 	#Another function render. 
 
 	#Render puts together the template. 
+
