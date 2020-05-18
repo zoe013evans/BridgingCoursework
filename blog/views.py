31e3789d742c70f5b028d221f1d9d47668673b5b
Zoe <ezoe013@gmail.com>
2020-05-18 18:01:38 +0100

    adding post stuff for html again

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,17 @@ from django.utils import timezone
 
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now().order_by('published_date'))
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+
+
+	#We're trying to connect mdodels and templates
+	#In a view we deicde what model will be displayed
+	#in a template
+
+
+
+	return render(request, 'blog/post_list.html', {'posts':posts})
 
 	#Creating a function (def) called post_list. 
 	#It takes a request and returns the value it gets from calling
