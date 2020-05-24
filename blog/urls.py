387ede6d6c2b14b26fe95c5496b9db2bedcfe567
Zoe <ezoe013@gmail.com>
2020-05-24 19:54:39 +0100

    Finishing off the security section of the blog woo

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 	path('post/new/', views.post_new, name='post_new'), 
-	path('post/<int:pk>/edit/', views.post_edit, name="post_edit")
+	path('post/<int:pk>/edit/', views.post_edit, name="post_edit"),
+	path('drafts/', views.post_draft_list, name='post_draft_list'),
+	path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+	path('post/<pk>/remove', views.post_remove, name='post_remove'), 
 ]
+
 
 #post/ means the URL should begin with word post followed by a / 
 #<int:pk> means an int is expected. This variable is called pk. 
