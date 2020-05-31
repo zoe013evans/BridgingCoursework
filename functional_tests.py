494f9a1b69e66e0e571a8b0a1fa79f20d531eb3d
Zoe <ezoe013@gmail.com>
2020-05-31 13:28:54 +0100

    got some of the functional tests to pass

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		self.assertIn('Cv Page', self.browser.title)
 		
-		header_text = self.browser.find_element_by_name('h1').text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('Cv Page', header_text)
 
 		#Invited to enter a to-do item straight away 
@@ -57,9 +57,10 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id(id_list_table)
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table")
 
 
 		self.fail('Finish the test')
